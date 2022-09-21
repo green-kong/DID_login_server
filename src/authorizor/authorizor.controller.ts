9afc13cd5422fc312da57ae5b22a2cc3da37e7df
dev_kong <ahdjd5@gmail.com>
2022-09-21 17:36:17 +0900

    style: rmv noneeds console.log)

--- src/authorizor/authorizor.controller.ts
+++ src/authorizor/authorizor.controller.ts
@@ -123,7 +123,6 @@ export class AuthorizorController {
     if (disconnectResult) {
       res.cookie('DID_ACCESS_TOKEN', '', { maxAge: 0 });
       res.cookie('DID_REFRESH_TOKEN', '', { maxAge: 0 });
-      console.log(disconnectResult);
       res.redirect(disconnectResult);
     } else {
       res.sendStatus(500).send(false);
